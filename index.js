3804e37047bff8dbba5e885dd429fe4949a8cc40
garryjoint <garryjoint@sohu.com>
2018-04-20 12:17:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello garry, the sec time");
+    response.end("Hello garry, the sect time");
 
 });
 
